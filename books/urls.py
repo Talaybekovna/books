1605f0e063cd40d81caf9c6ff41f061db07319f6
Kunduz <talaybekovna.kunduz@gmail.com>
2021-01-20 22:16:30 +0600

    connected styles to books list

--- books/urls.py
+++ books/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="homepage"),
+    path("books/", books, name="books"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
+from .models import Books
 
 def homepage(request):
     return render(request, "books.html")
+
+def books(request):
+    books_list = Books.objects.all()
+    return render(request, "books.html", {"books_list": books_list})
